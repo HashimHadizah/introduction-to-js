9964e2f2e0ec4b8e2f20b63d1863defcd34b54aa
HashimHadizah <53875760+HashimHadizah@users.noreply.github.com>
2022-06-24 16:40:58 -0400

    my third commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const myName = "My name is Hashim Hadizah. \n"
+const myHeight = " I am 5.7 inches tall. \n";
+const myCountry = " I am a citizen of Nigeria.";
+
+alert(myName + myHeight + myCountry)
+
+
+
